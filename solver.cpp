abf21d714d803cb65fb01b24e47211825ac0c352
Netanel Albert <netal90@gmail.com>
2020-05-05 18:45:44 +0300

    Move to template without inheriting. Need to fix linking error.

--- solver.cpp
+++ solver.cpp
@@ -2,91 +2,69 @@
 // Created by Netanel Albert on 26/04/2020.
 //
 
+#ifndef SOLVER_A_SOLVER_CPP
+#define SOLVER_A_SOLVER_CPP
 
 #include "solver.hpp"
-#define TEMPL_TYPE template <typename coe_type, typename class_type>
+
+#define TEMPL_TYPE template <typename coefficient_type>
 using namespace solver;
+using namespace std;
+
 TEMPL_TYPE
-class_type Variable<coe_type, class_type>::operator+(const class_type& other) const{
-    return class_type(_a + other._a, _b + other._b , _c + other._c);
-}
-TEMPL_TYPE
-class_type Variable<coe_type, class_type>::operator+(const coe_type& n) const{
-    return class_type(_a, _b, _c + n);
-}
-TEMPL_TYPE
-class_type Variable<coe_type, class_type>::operator-(const class_type& other) const{
-    return class_type(_a - other._a, _b - other._b , _c - other._c);
-}
-TEMPL_TYPE
-class_type Variable<coe_type, class_type>::operator-(const coe_type& n) const{
-    return class_type(_a, _b, _c - n);
+solver::VAR operator+(const VAR& v1,const VAR& v2){
+    return VAR(v1._a + v2._a, v1._b + v2._b , v1._c + v2._c);
 }
 
 TEMPL_TYPE
-class_type Variable<coe_type, class_type>::operator*(const class_type& other) const{
-    u_int power = 0;
-    if(_a != 0.0 ) power += 2;
-    if(_b != 0.0 ) power++;
-    if(other._a != 0.0 ) power += 2;
-    if(other._b != 0.0 ) power++;
-    if(power > 2)
-        throw runtime_error("Can handle only degree 1 or 2");
-
-    Variable<coe_type, class_type> xcopy =  *this;
-    xcopy._a = xcopy._b; xcopy._b = xcopy._c; xcopy._c = 0;
-
-    Variable<coe_type, class_type> xxcopy = *this;
-    xxcopy._a = xxcopy._c; xxcopy._b = 0; xxcopy._c = 0;
-
-    return ((*this)*other._c) + (xcopy*other._b)  + (xxcopy*other._a);
-}
-TEMPL_TYPE
-class_type Variable<coe_type, class_type>::operator*(const coe_type& n) const{
-    return class_type(_a * n, _b * n , _c * n);
-}
-TEMPL_TYPE
-class_type Variable<coe_type, class_type>::operator/(const class_type& other) const{
-    if (other._a == 0.0 && other._b == 0.0)
-        return *this/other._c;
+solver::VAR operator/(const VAR& v1, const VAR& other){
+    if (other._a == 0.0 && other._b == 0.0){
+        return VAR(v1._a / other._c, v1._b / other._c, v1._c / other._c);
+    }
     throw runtime_error("Cant divide by variable");
 }
 TEMPL_TYPE
-class_type Variable<coe_type, class_type>::operator/(const coe_type& n) const{
-    return class_type(_a / n, _b / n , _c / n);
+VAR operator-(const VAR& v1,const VAR& v2){
+    return VAR(v1._a - v2._a, v1._b - v2._b , v1._c - v2._c);
 }
-//// TODO make sure that can't get 1/x, 1/x^2
-
 TEMPL_TYPE
-class_type Variable<coe_type, class_type>::operator^(const int& n) const{
+VAR operator*(const VAR& v1,const VAR& v2){
+    u_int power = 0;
+    if(v1._a != 0.0 ) power += 2;
+    if(v1._b != 0.0 ) power++;
+    if(v2._a != 0.0 ) power += 2;
+    if(v2._b != 0.0 ) power++;
+    if(power > 2)
+        throw runtime_error("Can handle only degree 1 or 2");
+
+    VAR xcopy(v1._b, v1._c, 0);
+    VAR xxcopy(v1._c, 0, 0);
+    return (v1*v2._c) + (xcopy*v2._b)  + (xxcopy*v2._a);
+}
+TEMPL_TYPE
+VAR operator==(const VAR& v1,const VAR& v2){
+    return v1-v2;
+}
+TEMPL_TYPE
+VAR VAR::operator^(const int& n) const{
     switch (n){
         case 0:
-            return  class_type(0,0,1);
+            return  VAR(0,0,1);
         case 1:
             cout << "^1" << endl;
-            return *this+0;
+            return *this;
         case 2:
-            return (*this+0)*(*this+0);
+            return (*this)*(*this);
         default:
             throw runtime_error("can only handle power of 0,1 or 2");
     }
 }
 TEMPL_TYPE
-class_type Variable<coe_type, class_type>::operator==(const class_type& other) const{
-    return *this - other;
+VAR VAR::operator -() const{
+    return VAR(-_a, -_b, -_c);
 }
 TEMPL_TYPE
-class_type Variable<coe_type, class_type>::operator==(const coe_type& n) const{
-    return *this - n;
-}
-
-TEMPL_TYPE
-class_type Variable<coe_type, class_type>::operator -() const{
-    return class_type(-_a, -_b, -_c);
-}
-TEMPL_TYPE
-complex<double> Variable<coe_type, class_type>::compareToZero()  const{
-    //cout << "solve" << *this << endl;
+complex<double> VAR::compareToZero()  const{
     complex<double> ans;
     if(_a == 0.0 && _b == 0.0)
         throw runtime_error("there is no variable");
@@ -99,6 +77,7 @@ complex<double> Variable<coe_type, class_type>::compareToZero()  const{
     return ans;
 }
 
-template class solver::Variable<complex<double>, ComplexVariable>;
-template class solver::Variable<double, RealVariable>;
+//template class solver::Variable<complex<double>>;
+//template class solver::Variable<double>;
 
+#endif //SOLVER_A_SOLVER_CPP
--- solver.hpp
+++ solver.hpp
@@ -7,72 +7,43 @@
 
 #include <iostream>
 #include <complex>
+#define VAR Variable<coefficient_type>
+
 using namespace std;
+
 namespace solver{
     /**
      * This template class represents a quadratic expression of the form 'a*x^2 + b*x +c'
      * The default initialise creat just '1*x'
      *
      * @tparam coefficient_type - in my implementation - double or complex<double>
-     * @tparam class_type - the derived class.
      */
-    template <typename coefficient_type, class class_type>
+    template <typename coefficient_type>
     class Variable{
         coefficient_type _a;
         coefficient_type _b;
         coefficient_type _c;
     protected:
-        Variable (coefficient_type a, coefficient_type b, coefficient_type c): _a(a), _b(b), _c(c){};
+        Variable<coefficient_type>(coefficient_type a, coefficient_type b, coefficient_type c): _a(a), _b(b), _c(c){};
     public:
-        Variable (): _a(0), _b(1), _c(0){};
-        class_type operator+(const class_type&) const;
-        class_type operator+(const coefficient_type&) const;
-        class_type operator-(const class_type&) const;
-        class_type operator-(const coefficient_type&) const;
-        friend class_type operator+(const coefficient_type& n,const class_type& other){
-            return other + n;
-        }
-        friend class_type operator-(const coefficient_type& n,const class_type& other){
-            return other - n;
-        }
+        Variable<coefficient_type> (): _a(0), _b(1), _c(0){};
+        Variable<coefficient_type> (const coefficient_type& c): _a(0), _b(0), _c(c){};
+        Variable<coefficient_type> (const int& c): _a(0), _b(0), _c(1.0*c){};
 
-        class_type operator*(const class_type&) const;
-        class_type operator*(const coefficient_type&) const;
-        class_type operator/(const class_type&) const;
-        class_type operator/(const coefficient_type&) const;
-        friend class_type operator*(const coefficient_type& n,const class_type& other){
-            return other * n;
-        }
-        friend class_type operator/(const coefficient_type& n, const class_type& other){
-            if (other._a == 0.0 && other._b == 0.0){
-                return class_type(0, 0, n/other._c);
-            }
-            throw runtime_error("Cant divide by variable");
-        }
+        friend VAR operator+(const VAR& n,const VAR& other);
+        friend VAR operator-(const VAR& n,const VAR& other);
+        VAR operator -() const;
 
+        friend VAR operator*(const VAR& n,const VAR& other);
+        friend VAR operator/(const VAR& n, const VAR& other);
 
-        class_type operator^(const int&) const;
-        class_type operator==(const class_type&) const;
-        class_type operator==(const coefficient_type &) const;
-        friend class_type operator==(const coefficient_type& n, const class_type& other) {
-            return other == n;
-        }
-
-        class_type operator -() const;
+        VAR operator^(const int&) const;
+        friend VAR operator==(const VAR&, const VAR&);
         complex<double> compareToZero() const;
     };
 
-    class ComplexVariable: public Variable<complex<double>, ComplexVariable>{
-    public:
-        ComplexVariable() : Variable<complex<double>, ComplexVariable>() {};
-        ComplexVariable (complex<double> a, complex<double> b, complex<double> c): Variable<complex<double>, ComplexVariable>(a,b,c) {};
-    };
-
-    class RealVariable: public Variable<double, RealVariable>{
-    public:
-        RealVariable (): Variable<double, RealVariable>() {};
-        RealVariable (double a, double b, double c): Variable<double, RealVariable>(a,b,c) {};
-    };
+    using RealVariable = Variable<double>;
+    using ComplexVariable = Variable<complex<double>>;
 
     inline double solve(const RealVariable& var){
         complex<double>  ans = var.compareToZero();
@@ -80,11 +51,13 @@ namespace solver{
             throw runtime_error("the equation has no real solution");
 
         return ans.real();
-    };
+    }
     inline complex<double> solve(const ComplexVariable& var){
         return var.compareToZero();
-    };
+    }
 
 }
 
 #endif //SOLVER_A_SOLVER_H
+
+#include "solver.cpp"
